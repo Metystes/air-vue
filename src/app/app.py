3aeb9c1856fd7b241666aea51e76a5c9fbc5520c
Michał Wójcik <m.wojcik321@gmail.com>
2019-12-24 08:37:22 +0100

    Correcting app.py

--- src/app/app.py
+++ src/app/app.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template
 import plotly
 import json
 
-import pandas as pd
-
 # Import local modules
 from src.data.createDB import (Database, db_backend, db_name)
 
@@ -18,7 +16,8 @@ def get_data():
         SELECT m.*, ROW_NUMBER() OVER(PARTITION BY id ORDER BY time DESC) as rm
         FROM `airly_raw_data` as m
     ), locs as (SELECT id, lat, lng, city, street FROM 'airly_locs')
-    SELECT r.*, l.lat, l.lng FROM ranked r LEFT JOIN locs as l on l.id = r.id where rm = 1
+    SELECT r.*, l.lat, l.lng
+    FROM ranked r LEFT JOIN locs as l on l.id = r.id where rm = 1
     """
     return dbms.pd_readSQL(sql=query)
 
@@ -27,7 +26,8 @@ def plot_map(data):
     """Plotting new map
     
     Arguments:
-        data {df} -- [dataframe containing at leat 3 columns: Latitude, Longitude, value]
+        data {df} -- [dataframe containing at leat 3 columns:
+                      Latitude, Longitude, value]
     
     Returns:
         json -- JSON file with info about plotly figure
@@ -36,7 +36,9 @@ def plot_map(data):
                                      z=data.pm10,
                                      radius=12))
     fig.update_layout(mapbox_style="open-street-map",
-                      mapbox_center_lon=19.947015, mapbox_center_lat=50.055163, mapbox_zoom=8)
+                      mapbox_center_lon=19.947015,
+                      mapbox_center_lat=50.055163,
+                      mapbox_zoom=8)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     fig.layout.template = None
 
